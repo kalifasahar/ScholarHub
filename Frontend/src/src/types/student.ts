b9fa186f6ecffb020b7b246a20c3908277b6a1fd
sahar kalifa <kalifasahar>
2024-05-28 23:07:42 +0300

    delete files

---
+++ Frontend/src/src/types/student.ts
@@ -0,0 +1,25 @@
+import { CustomFile } from 'src/components/upload';
+
+// ----------------------------------------------------------------------
+
+export type IUserTableFilterValue = string | string[];
+
+export type IUserTableFilters = {
+  name: string;
+  role: string[];
+  status: string;
+};
+
+// ----------------------------------------------------------------------
+
+export type IStudentItem = {
+  name: string;
+  studentID: string;
+  email: string;
+  phoneNumber: string;
+  department: string,
+  degree: string;
+  status: string;
+  gradesAvarage: string;
+  numOfArticles: string;
+};
